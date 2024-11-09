b2a33f1e036c755194b84e2cd74de44d5688dea9
Stormwatcher <100471082+camuflory@users.noreply.github.com>
2024-11-09 17:27:19 +0300

    refactor

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         while (true) {
-            System.out.println(1);
+            System.out.println(2);
         }
     }
 }
